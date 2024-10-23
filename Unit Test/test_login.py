ca75267fa10ffa3586b02f3814664a3a78ad0a07
elejedomarygwyneth <151907178+elejedomarygwyneth@users.noreply.github.com>
2024-10-24 00:01:37 +0800

    Create test_login.py

---
+++ Unit Test/test_login.py
@@ -0,0 +1,12 @@
+import unittest
+from bl.login_bl import validate_login
+
+class TestLogin(unittest.TestCase):
+    def test_valid_login(self):
+        self.assertTrue(validate_login('admin', 'password'))
+
+    def test_invalid_login(self):
+        self.assertFalse(validate_login('wrong_user', 'wrong_password'))
+
+if __name__ == '__main__':
+    unittest.main()
