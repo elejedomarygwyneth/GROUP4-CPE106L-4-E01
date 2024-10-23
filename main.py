d63709a13a73fd549f420198cf205b67628cbb1a
elejedomarygwyneth <151907178+elejedomarygwyneth@users.noreply.github.com>
2024-10-24 00:02:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from flask import Flask
-from config import Config
-from dal.dal import db
-from ui.login_ui import setup_login_routes
-from ui.dashboard_ui import setup_dashboard_routes
+from ui import login_ui
+from data import db_connection
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
-
-setup_login_routes(app)
-setup_dashboard_routes(app)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    db_connection.create_tables()  # Initialize the database
+    # Start the login UI
+    login_ui.root.mainloop()
 
