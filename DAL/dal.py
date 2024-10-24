597d67fc86e1717cdb85957dead9ea153854b830
elejedomarygwyneth <151907178+elejedomarygwyneth@users.noreply.github.com>
2024-10-24 21:08:40 +0800

    Update dal.py

--- DAL/dal.py
+++ DAL/dal.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Event(db.Model):
+    __tablename__ = 'event'  # Explicitly name the table
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     date = db.Column(db.String(50), nullable=False)
