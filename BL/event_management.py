f895ef012e4ede1fa13c8d777e88f30eaf129fc9
elejedomarygwyneth <151907178+elejedomarygwyneth@users.noreply.github.com>
2024-10-24 20:24:16 +0800

    Update event_management.py

--- BL/event_management.py
+++ BL/event_management.py
@@ -1,14 +1,22 @@
-events = []
+from DAL.dal import db, Event
 
-def add_event(event_name, event_date):
-    event = {"name": event_name, "date": event_date}
-    events.append(event)
+def add_event(event_name, event_date, location, description, user_id):
+    event = Event(name=event_name, date=event_date, location=location, description=description, user_id=user_id)
+    db.session.add(event)
+    db.session.commit()
     return event
 
-def edit_event(event_name, new_name, new_date):
-    for event in events:
-        if event["name"] == event_name:
-            event["name"] = new_name
-            event["date"] = new_date
-            return event
+def edit_event(event_id, new_name, new_date, new_location, new_description):
+    event = Event.query.get(event_id)
+    if event:
+        event.name = new_name
+        event.date = new_date
+        event.location = new_location
+        event.description = new_description
+        db.session.commit()
+        return event
     return None
+
+def get_all_events(user_id):
+    return Event.query.filter_by(user_id=user_id).all()
+
