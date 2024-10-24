1e6969091be5125b8feff85bf0e74cfbc8d8e463
elejedomarygwyneth <151907178+elejedomarygwyneth@users.noreply.github.com>
2024-10-24 20:47:08 +0800

    Update dal.py

--- DAL/dal.py
+++ DAL/dal.py
@@ -2,18 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     date = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.String(150), nullable=False)
 
 
 
