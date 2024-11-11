4d7722044e2d02efbda49d12d0cb738fd40ae6cb
SamDelaCruz467 <156177657+SamDelaCruz467@users.noreply.github.com>
2024-11-11 21:46:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-import tkinter as tk
-from UI.login_ui import open_login
-
-if __name__ == "__main__":
-    open_login()
+import flet as ft
+from UI.login_ui import open_login_form
 
 
+def open_welcome_screen(page: ft.Page):
+    page.controls.clear()  # Clear previous content
+    page.title = "Welcome to EventSynch!"
+
+
+    def get_started(e):
+        open_login_form(page)
+
+
+    welcome_content = ft.Container(
+        content=ft.Column(
+            [
+                ft.Text("Welcome to EventSynch!", size=24, weight="bold", text_align=ft.TextAlign.CENTER),
+                ft.FilledButton("Get Started", on_click=get_started)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=20  # Adds spacing between elements
+        ),
+        alignment=ft.alignment.center,
+        padding=ft.padding.all(20),
+        expand=True  # Ensures full height and width expansion for centering
+    )
+    page.add(welcome_content)
+    page.update()
+
+
+def main(page: ft.Page):
+    open_welcome_screen(page)
+
+
+ft.app(target=main)
